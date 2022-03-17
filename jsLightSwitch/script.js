1a22b0b110d0cd343420c21f973e1d19b0ff83f3
Sam Fortuin <99059050@mydavinci.nl>
2022-03-17 10:57:19 +0100

    finished jsLightswitch

--- jsLightSwitch/script.js
+++ jsLightSwitch/script.js
@@ -5,6 +5,24 @@ document.body.appendChild(btn);
 
 // schijf hier tussen je code
 
+// init
+btn.innerHTML = 'Switch light ON';
+document.body.style.backgroundColor = 'black';
+btn.value = 'off';
+
+btn.addEventListener('click', ()=>{
+    if (btn.value === 'off'){
+        btn.innerHTML = 'Switch light OFF'
+        btn.value = 'on';
+        document.body.style.backgroundColor = 'yellow';
+    }
+    else if (btn.value === 'on'){
+        btn.value = 'off';
+        document.body.style.backgroundColor = 'black';
+        btn.innerHTML = 'Switch light ON'
+    }
+    console.log(btn.value)
+})
 
 
 // schijf hier tussen je code
