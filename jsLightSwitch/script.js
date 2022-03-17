bff1e0d491b98b9ff6bebdd4c96357c966f6c3d5
Sam Fortuin <99059050@mydavinci.nl>
2022-03-17 11:12:39 +0100

    added nakijk

--- jsLightSwitch/script.js
+++ jsLightSwitch/script.js
@@ -3,7 +3,7 @@ btn.style.margin = '10px';
 btn.innerHTML = '...';
 document.body.appendChild(btn);
 
-// schijf hier tussen je code
+//<schijf hier tussen je code>
 
 // init
 btn.innerHTML = 'Switch light ON';
@@ -12,17 +12,16 @@ btn.value = 'off';
 
 btn.addEventListener('click', ()=>{
     if (btn.value === 'off'){
-        btn.innerHTML = 'Switch light OFF'
+        btn.innerHTML = 'Switch light OFF';
         btn.value = 'on';
         document.body.style.backgroundColor = 'yellow';
     }
     else if (btn.value === 'on'){
         btn.value = 'off';
         document.body.style.backgroundColor = 'black';
-        btn.innerHTML = 'Switch light ON'
+        btn.innerHTML = 'Switch light ON';
     }
-    console.log(btn.value)
+    console.log(btn.value);
 })
 
-
-// schijf hier tussen je code
+//<schijf hier tussen je code\>
